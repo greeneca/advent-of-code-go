3fb3196468e1697116ce8aa3144af4ae72149382
Charles Greene <charles.greene@redspace.com>
2025-04-22 15:48:32 -0300

    Add Day 1 2017

--- aoc2017/aoc2017.go
+++ aoc2017/aoc2017.go
@@ -8,6 +8,7 @@ import (
 func RunProblem(day int, part int, data []string) error {
 	problems := map[string]func([]string){
 		"day1Part1": day1Part1,
+		"day1Part2": day1Part2,
 		// Add other problems here
 	}
 	functionName := fmt.Sprintf("day%dPart%d", day, part)
--- aoc2017/day1.go
+++ aoc2017/day1.go
@@ -1,8 +1,38 @@
 package aoc2017
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func day1Part1(data []string) {
-	fmt.Println("Day 1 Part 1")
-	// Implement the solution for Day 1 Part 1 here
+	input := data[0]
+	prev := string(input[len(input)-1])
+	sum := 0
+	for i := range len(input) {
+		current := string(input[i])
+		if current == prev {
+			value, _ := strconv.Atoi(current)
+			sum += value
+		}
+		prev = current
+	}
+	fmt.Println("Sum of matching digits:", sum)
+}
+
+func day1Part2(data []string) {
+	input := data[0]
+	length := len(input)
+	halfway := length / 2
+	sum := 0
+
+	for i := range length {
+		current := string(input[i])
+		next := string(input[(i+halfway)%length])
+		if current == next {
+			value, _ := strconv.Atoi(current)
+			sum += value
+		}
+	}
+	fmt.Println("Sum of matching digits:", sum)
 }
