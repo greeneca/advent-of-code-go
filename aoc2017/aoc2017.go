4e49dcda43352e5e9fa75c3423adbef5b6dc6101
Charles Greene <charles.greene@redspace.com>
2025-04-24 09:30:19 -0300

    Add 2017 day 2

--- aoc2017/aoc2017.go
+++ aoc2017/aoc2017.go
@@ -9,6 +9,8 @@ func RunProblem(day int, part int, data []string) error {
 	problems := map[string]func([]string){
 		"day1Part1": day1Part1,
 		"day1Part2": day1Part2,
+		"day2Part1": day2Part1,
+		"day2Part2": day2Part2,
 		// Add other problems here
 	}
 	functionName := fmt.Sprintf("day%dPart%d", day, part)
---
+++ aoc2017/day2.go
@@ -0,0 +1,56 @@
+package aoc2017
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func day2Part1(data []string) {
+	sum := 0
+	for i := range len(data) {
+		line := string(data[i])
+		min := 9999
+		max := 0
+		values := strings.Split(line, "\t")
+		for j := range len(values) {
+			value, _ := strconv.Atoi(values[j])
+			if value < min {
+				min = value
+			}
+			if value > max {
+				max = value
+			}
+		}
+		sum += max - min
+	}
+	fmt.Println("Checksum:", sum)
+}
+
+func day2Part2(data []string) {
+	sum := 0
+	for i := range len(data) {
+		line := string(data[i])
+		values := strings.Split(line, "\t")
+		for j := range len(values) {
+			toAdd := -1
+			value, _ := strconv.Atoi(values[j])
+			for k := j+1; k < len(values); k++ {
+				value2, _ := strconv.Atoi(values[k])
+				if value%value2 == 0 {
+					toAdd = value / value2
+					break
+				}
+				if value2%value == 0 {
+					toAdd = value2 / value
+					break
+				}
+			}
+			if toAdd != -1 {
+				sum += toAdd
+				break
+			}
+		}
+	}
+	fmt.Println("Checksum:", sum)
+}
