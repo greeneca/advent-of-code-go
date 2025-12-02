5fffc9129530d0475058486acc82dbbd0743eba8
Charles Greene <charles.greene@redspace.com>
2025-12-02 09:11:02 -0400

    Add Day 2 2025

---
+++ aoc2025/day2.go
@@ -0,0 +1,61 @@
+package aoc2025
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func day2Part1(data []string) {
+	sum := 0
+	ranges := strings.SplitSeq(data[0], ",")
+	for r := range ranges {
+		bounds := strings.Split(r, "-")
+		start, _ := strconv.Atoi(bounds[0])
+		end, _ := strconv.Atoi(bounds[1])
+		println("Processing range:", start, "to", end)
+		for i := start; i <= end; i++ {
+			strNum := strconv.Itoa(i)
+			length := len(strNum)
+			if strNum[0:(length)/2] == strNum[(length)/2:] {
+				println("Found invalid id:", strNum)
+				sum += i
+			}
+		}
+	}
+	fmt.Println("Sum is:", sum)
+}
+
+func day2Part2(data []string) {
+	sum := 0
+	ranges := strings.SplitSeq(data[0], ",")
+	for r := range ranges {
+		bounds := strings.Split(r, "-")
+		start, _ := strconv.Atoi(bounds[0])
+		end, _ := strconv.Atoi(bounds[1])
+		for i := start; i <= end; i++ {
+			strNum := strconv.Itoa(i)
+			length := len(strNum)
+			for j := 1; j <= length/2; j++ {
+				if length%(j) != 0 { continue }
+				matching := true
+				for k := 0; k <= (length-(2*j)); k+= j {
+					a1 := k
+					a2 := k + (j)
+					b1 := k + j
+					b2 := k + (2 * j)
+
+					if strNum[a1:a2] != strNum[b1:b2] {
+						matching = false
+						break
+					}
+				}
+				if matching {
+					sum += i
+					break
+				}
+			}
+		}
+	}
+	fmt.Println("Sum is:", sum)
+}
